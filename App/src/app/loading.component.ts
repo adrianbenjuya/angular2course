978161fbab32bea6bd75731b5660359c8cfbcada
Adrián <adrian.benjuya@hotmail.es>
2017-02-24 18:17:39 -0300

    Changed loading img

--- App/src/app/loading.component.ts
+++ App/src/app/loading.component.ts
@@ -2,7 +2,7 @@ import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'loading',
-  template: '<div id="loading"><img src="assets/images/loading.png" class="{{theme}}" /></div>',
+  template: '<div id="loading" class="{{theme}}"><i class="glyphicon glyphicon-refresh"></i></div>',
   styleUrls: ['./loading.component.css']
 })
 export class LoadingComponent {
