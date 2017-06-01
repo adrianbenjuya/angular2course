e3ee4c351231ffd88bc035bf013720d4ce218299
Adrián <adrian.benjuya@hotmail.es>
2017-05-31 22:40:50 -0300

    Changed api url

--- App/src/app/app.config.ts
+++ App/src/app/app.config.ts
@@ -1,8 +1,8 @@
 export class AppConfig {
 
-    public static APP_SERVICE_URL: string = 'http://acntest.brazilsouth.cloudapp.azure.com/api';
+    public static APP_SERVICE_URL: string = 'http://appserverclon.cloudapp.azure.com/TourOfHeroes/api';
 
-    public static TEST_ENVIRONMENT: boolean = true;
+    public static TEST_ENVIRONMENT: boolean = false;
 
     public static HEROES_URL: string = (AppConfig.TEST_ENVIRONMENT ? 'http://localhost:54791/api' : AppConfig.APP_SERVICE_URL) + '/heroes/'
 }
