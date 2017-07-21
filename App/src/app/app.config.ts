86972baeb66e308048e48e1dbb74df0226cdefab
Adrián <adrian.benjuya@hotmail.es>
2017-07-20 22:33:35 -0300

    Removed app config file

--- App/src/app/app.config.ts
+++
@@ -1,8 +0,0 @@
-// export class AppConfig {
-
-//     public static APP_SERVICE_URL: string = 'http://appserverclon.cloudapp.net/TourOfHeroes/api';
-
-//     public static TEST_ENVIRONMENT: boolean = false;
-
-//     public static HEROES_URL: string = (AppConfig.TEST_ENVIRONMENT ? 'http://localhost:54791/api' : AppConfig.APP_SERVICE_URL) + '/heroes/'
-// }
